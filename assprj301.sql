18c995d7646c78d0ce1d57cf6797c554dec18edd
FnPrtScr <tn4490523@gmail.com>
2022-08-19 02:06:23 +0700

    update sql

--- assprj301.sql
+++ assprj301.sql
@@ -8,13 +8,19 @@ select e.eid,[name],office,lfrom,lto,reason from Employee e inner join Leave l
 on l.eid= e.eid
 
 
---tong so ngay di lam
+--dem so ngay di lam
 select COUNT(Working.eid) as allworking
 from Working
 where eid=?
 
 
---tinh tong ngay nghi
+--dem so ngay nghi
 select COUNT(Leave.eid) as allleave
 from Leave
 where eid=?
+
+
+--hien thi luong cua nhan vien bat ky
+select e.eid,[name],office,s.salary from Employee e inner join SalaryOffice s 
+on s.eid=e.eid
+where e.eid=?
