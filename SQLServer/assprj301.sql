603f6b4578ac1f42885bd0ab2d711f8cb77ef66d
FnPrtScr <tn4490523@gmail.com>
2022-08-19 04:41:03 +0700

    UPDATE SQL

--- SQLServer/assprj301.sql
+++ SQLServer/assprj301.sql
@@ -1,28 +1,33 @@
+
 --hien thi nhung ngay di lam
-select e.eid,[name],office,wfrom,wto from Employee e inner join Working w
+select e.eid,[name],office,w.wdate from Employee e inner join Working w
 on w.eid = e.eid
 
 
 --hien thi nhung ngay nghi
-select e.eid,[name],office,lfrom,lto,reason from Employee e inner join Leave l
+select e.eid,[name],office,l.ldate,reason,acceptleave from Employee e inner join Leave l
 on l.eid= e.eid
 
 
 --dem so ngay di lam
-select COUNT(Working.eid) as allworking
+select COUNT(Working.eid) as countworking
 from Working
 where eid=?
 
 
 --dem so ngay nghi
-select COUNT(Leave.eid) as allleave
+select COUNT(Leave.eid) as countleave
 from Leave
 where eid=?
 
 
---
-
 --hien thi luong cua nhan vien bat ky
-select e.eid,[name],office,s.salary from Employee e inner join SalaryOffice s 
+select e.eid,[name],office,s.salaryoffice from Employee e inner join SalaryOffice s 
 on s.eid=e.eid
+where e.eid=?
+
+
+--hien thi so tien ung cua nhan vien bat ky
+select e.eid,[name],office,sa.datesa,sa.howmuchsa,sa.reasonsa,sa.acceptsa from Employee e inner join SalaryAdvance sa
+on sa.eid=e.eid
 where e.eid=?
